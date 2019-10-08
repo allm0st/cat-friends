3a27df8f9512d88ccd3b268797da367c35e4ea03
AlLmOsTheRE <tiago_azevedo_25@hotmail.com>
2019-10-08 20:41:44 +0100

    Updates

---
+++ precache-manifest.e4d4de64eacff51f4d69eb757f4b5964.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235aac62a00441dabb36f364eb9b4c87",
+    "url": "/cat-friends/index.html"
+  },
+  {
+    "revision": "5ce43151855f2ed1a651",
+    "url": "/cat-friends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "64ef157a2c5dff780855",
+    "url": "/cat-friends/static/css/main.e8ea7b5f.chunk.css"
+  },
+  {
+    "revision": "5ce43151855f2ed1a651",
+    "url": "/cat-friends/static/js/2.c0fc6d73.chunk.js"
+  },
+  {
+    "revision": "64ef157a2c5dff780855",
+    "url": "/cat-friends/static/js/main.f4858d00.chunk.js"
+  },
+  {
+    "revision": "af9605499de54bb27819",
+    "url": "/cat-friends/static/js/runtime~main.cbd533e9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/cat-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
