d87d992588fc4754401b1134fe6b900403a6689e
AlLmOsTheRE <36304262+AlLmOsTheRE@users.noreply.github.com>
2022-05-25 17:43:49 +0100

    fixes robohash API request

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ id, name, email }) => (
   <div className="bg-light-green dib br3 pa3 ma2 grow tc">
-    <img alt={`User: ${name}`} src={`https://robohash.org/${id}?set=set4&size=200x200`} />
+    <img alt={`User: ${name}`} src={`https://robohash.org/${id}?set=set4`} height="200" width="200" />
     <div>
       <h2>{name}</h2>
       <p>{email}</p>
